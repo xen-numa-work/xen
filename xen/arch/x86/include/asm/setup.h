5dbb94e3b3faca88a02f6d2368cfe8cced220236
Roger Pau Monne <roger.pau@citrix.com>
2024-01-25 14:26:26 +0100

    From 79fcc0e9d7df3a53cf69880729a43c1a8d3d8631 Mon Sep 17 00:00:00 2001 x86/iommu: remove regions not to be mapped
    MIME-Version: 1.0
    Content-Type: text/plain; charset=UTF-8
    Content-Transfer-Encoding: 8bit
    
    Introduce the code to remove regions not to be mapped from the rangeset
    that will be used to setup the IOMMU page tables for the hardware domain.
    
    This change also introduces two new functions: remove_xen_ranges() and
    vpci_subtract_mmcfg() that copy the logic in xen_in_range() and
    vpci_is_mmcfg_address() respectively and remove the ranges that would otherwise
    be intercepted by the original functions.
    
    Note that the rangeset is still not populated.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Paul Durrant <paul@xen.org>

--- xen/arch/x86/include/asm/setup.h
+++ xen/arch/x86/include/asm/setup.h
@@ -44,6 +44,7 @@ void discard_initial_images(void);
 void *bootstrap_map(const module_t *mod);
 
 int xen_in_range(unsigned long mfn);
+int remove_xen_ranges(struct rangeset *r);
 
 void microcode_grab_module(
     unsigned long *, const multiboot_info_t *);
