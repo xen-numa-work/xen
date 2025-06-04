xen-numa-work/xen

commit 318ac791f9f91fa99ea92bee051e144874bdcb00

Add utilities needed for SMBIOS generation to hvmloader. Signed-off-by: Andrew D. Ball <aball@us.ibm.com>

tools/firmware/hvmloader/util.c
@@ -20,6 +20,7 @@
 
 #include "../acpi/acpi2_0.h"  /* for ACPI_PHYSICAL_ADDRESS */
 #include "util.h"
+#include <stdint.h>
 
 void outw(uint16_t addr, uint16_t val)
 {
@@ -94,3 +95,82 @@ void puts(const char *s)
 	while (*s)
 		outb(0xE9, *s++);
 }
+
+char *
+strcpy(char *dest, const char *src)
+{
+	char *p = dest;
+	while (*src)
+		*p++ = *src++;
+	*p = 0;
+	return dest;
+}
+
+char *
+strncpy(char *dest, const char *src, unsigned n)
+{
+	int i = 0;
+	char *p = dest;
+
+	/* write non-NUL characters from src into dest until we run
+	   out of room in dest or encounter a NUL in src */
+	while (i < n && *src) {
+		*p++ = *src++;
+		++i;
+	}
+
+	/* pad remaining bytes of dest with NUL bytes */
+	while (i < n) {
+		*p++ = 0;
+		++i;
+	}
+
+	return dest;
+}
+
+unsigned
+strlen(const char *s)
+{
+	int i = 0;
+	while (*s++)
+		++i;
+	return i;
+}
+
+void *
+memset(void *s, int c, unsigned n)
+{
+	uint8_t b = (uint8_t) c;
+	uint8_t *p = (uint8_t *)s;
+	int i;
+	for (i = 0; i < n; ++i)
+		*p++ = b;
+	return s;
+}
+
+int
+memcmp(const void *s1, const void *s2, unsigned n)
+{
+	unsigned i;
+	uint8_t *p1 = (uint8_t *) s1;
+	uint8_t *p2 = (uint8_t *) s2;
+
+	for (i = 0; i < n; ++i) {
+		if (p1[i] < p2[i])
+			return -1;
+		else if (p1[i] > p2[i])
+			return 1;
+	}
+
+	return 0;
+}
+
+void
+cpuid(uint32_t idx, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx)
+{
+	__asm__ __volatile__(
+		"cpuid"
+		: "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
+		: "0" (idx) );
+}
+

tools/firmware/hvmloader/util.h
@@ -8,9 +8,21 @@ void outb(uint16_t addr, uint8_t val);
 /* I/O input */
 uint8_t inb(uint16_t addr);
 
+/* Do cpuid instruction, with operation 'idx' */
+void cpuid(uint32_t idx, uint32_t *eax, uint32_t *ebx,
+           uint32_t *ecx, uint32_t *edx);
+
+/* Return number of vcpus. */
+int get_vcpu_nr(void);
+
 /* String and memory functions */
 int strcmp(const char *cs, const char *ct);
+char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, unsigned n);
+unsigned strlen(const char *s);
+int memcmp(const void *s1, const void *s2, unsigned n);
 void *memcpy(void *dest, const void *src, unsigned n);
+void *memset(void *s, int c, unsigned n);
 char *itoa(char *a, unsigned int i);
 
 /* Debug output */

tools/firmware/hvmloader/hvmloader.c
@@ -116,15 +116,6 @@ check_amd(void)
 }
 
 static void
-cpuid(uint32_t idx, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx)
-{
-	__asm__ __volatile__(
-		"cpuid"
-		: "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
-		: "0" (idx) );
-}
-
-static void
 wrmsr(uint32_t idx, uint64_t v)
 {
 	__asm__ __volatile__(

commit facd1f2191575b75d92b89d51af2bd6ee137a2ce

From cc47813c4a2c07a5c6c6a1491b98f3f8549835a7 Mon Sep 17 00:00:00 2001 tools/hvmloader: Fix non-deterministic cpuid()
hvmloader's cpuid() implementation deviates from Xen's in that the value
passed on ecx is unspecified. This means that when used on leaves that
implement subleaves it's unspecified which one you get; though it's more
than likely an invalid one.

Import Xen's implementation so there are no surprises.

Fixes: 318ac791f9f9 ("Add utilities needed for SMBIOS generation to hvmloader")
Signed-off-by: Alejandro Vallejo <alejandro.vallejo@cloud.com>
Reviewed-by: Jan Beulich <jbeulich@suse.com>

tools/firmware/hvmloader/util.c
@@ -267,15 +267,6 @@ memcmp(const void *s1, const void *s2, unsigned n)
     return 0;
 }
 
-void
-cpuid(uint32_t idx, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx)
-{
-    asm volatile (
-        "cpuid"
-        : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
-        : "0" (idx) );
-}
-
 static const char hex_digits[] = "0123456789abcdef";
 
 /* Write a two-character hex representation of 'byte' to digits[].

tools/firmware/hvmloader/util.h
@@ -199,9 +199,30 @@ int uart_exists(uint16_t uart_base);
 int lpt_exists(uint16_t lpt_base);
 int hpet_exists(unsigned long hpet_base);
 
-/* Do cpuid instruction, with operation 'idx' */
-void cpuid(uint32_t idx, uint32_t *eax, uint32_t *ebx,
-           uint32_t *ecx, uint32_t *edx);
+/* Some CPUID calls want 'count' to be placed in ecx */
+static inline void cpuid_count(
+    uint32_t leaf,
+    uint32_t subleaf,
+    uint32_t *eax,
+    uint32_t *ebx,
+    uint32_t *ecx,
+    uint32_t *edx)
+{
+    asm volatile ( "cpuid"
+                   : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
+                   : "a" (leaf), "c" (subleaf) );
+}
+
+/* Generic CPUID function (subleaf 0) */
+static inline void cpuid(
+    uint32_t leaf,
+    uint32_t *eax,
+    uint32_t *ebx,
+    uint32_t *ecx,
+    uint32_t *edx)
+{
+    cpuid_count(leaf, 0, eax, ebx, ecx, edx);
+}
 
 /* Read the TSC register. */
 static inline uint64_t rdtsc(void)